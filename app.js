660f43ba6adc721f5527c76beee56393f1503677
Deepanker Tiwari <deepamamazing@gmail.com>
2025-12-13 10:30:00 +0530

    Revision Nodejs Continue

--- app.js
+++ app.js
@@ -6,5 +6,15 @@
 
 
 const add=require("./sum");
+const test=require("./testing")
+// const {calculateSum,x}=require("./testing")      //Avoid if x is more than 1 time in the program
+
 
 add(1,2)
+
+const x=100
+
+console.log(test.x);
+
+// console.log("Imported x: ",x)   // It will give an error to avoid this i don't have to destructure on the fly
+console.log(x);
---
+++ testing.js
@@ -0,0 +1,8 @@
+
+const calculateSum=function(a,b){
+    console.log(a+b);
+}
+
+const x=10
+
+module.exports={calculateSum:calculateSum,x:x};
