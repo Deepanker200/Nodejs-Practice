30b8b1a328d95ed4d5b2b94b6ebc7caf64516e75
Deepanker Tiwari <deepamamazing@gmail.com>
2025-12-22 10:30:00 +0530

    Nodejs practice~ Libuv working hardcore

--- libuv-event-loop.js
+++ libuv-event-loop.js
@@ -1,4 +1,4 @@
-//4th example
+//This example shows the event loop working in Libuv
 
 const fs = require("fs");
 
@@ -9,13 +9,13 @@ setTimeout(() => console.log("Timer expired"), 0);
 Promise.resolve("Promise").then(console.log);       //Promise value is passed in console.log
 
 
-fs.readFile("./file.txt", "utf8", () => {
+fs.readFile("./data.txt", "utf8", () => {
     console.log("File Reading CB");
 });
 
 process.nextTick(() => {
     process.nextTick(() => console.log("inner nextTick"));
-    // setTimeout(() => console.log("setTimeOut of nextTick"),0);     It wil print after Timer Expired
+    setTimeout(() => console.log("setTimeOut of nextTick"),0);     //It wil print after setImmediate because it sends it to other Callback queue which will run in next turn
 
     console.log("nextTick");
 })
